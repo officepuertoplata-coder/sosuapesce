9ad7ca205bba6c1bbcc441aa2a6d68ae4f2260e4
officepuertoplata-coder <officepuertoplata@gmail.com>
2026-04-25 21:20:08 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var SP_CONFIG = {
   SUPABASE_URL:         "https://oxybanupdrfopjionekk.supabase.co",
-  SUPABASE_ANON:        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94eWJhbnVwZHJmb3BqaW9uZWtrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcwNTg1MjMsImV4cCI6MjA5MjYzNDUyM30.vmssHhb5u2myX29SAPb496kOfYIEBmnus5ylpe-Z7Uc",
-  SUPABASE_SERVICE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94eWJhbnVwZHJmb3BqaW9uZWtrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NzA1ODUyMywiZXhwIjoyMDkyNjM0NTIzfQ.AqbI5tqSLcAGH0SzqPbYPds3GmiQYerXJ17H74ismz4",
+  SUPABASE_ANON:        "DEIN_PUBLISHABLE_KEY_HIER",
+  SUPABASE_SERVICE_KEY: "DEIN_SERVICE_KEY_HIER",
   SUPERADMIN_PASSWORD:  "Tq2MTPzno1DsCEH8",
   ADMIN_PASSWORD:       "Austria714#",
   SITE_URL:             "https://officepuertoplata-coder.github.io/sosuapesce"
