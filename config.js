143af7339f0b3ebdde5ce93043bb280ddd015cdb
officepuertoplata-coder <officepuertoplata@gmail.com>
2026-04-27 09:40:05 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 var SP_CONFIG = {
   SUPABASE_URL:         "https://oxybanupdrfopjionekk.supabase.co",
   SUPABASE_ANON:        "sb_publishable_qt8l3wl3IZSRAqKD2g6NeQ_0IZv_8Iy",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94eWJhbnVwZHJmb3BqaW9uZWtrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcwNTg1MjMsImV4cCI6MjA5MjYzNDUyM30.vmssHhb5u2myX29SAPb496kOfYIEBmnus5ylpe-Z7Uc",
   SUPABASE_SERVICE_KEY: "sb_secret_LpJWkjPyzJQ0ltxKf7tvVg_U7I8C2Md",
   SUPERADMIN_PASSWORD:  "Tq2MTPzno1DsCEH8",
   ADMIN_PASSWORD:       "Austria714#",
