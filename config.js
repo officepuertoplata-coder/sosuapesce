951eb9202a0fceff9d4fb9e648d05219b1ef3277
officepuertoplata-coder <officepuertoplata@gmail.com>
2026-05-01 12:08:16 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var SP_CONFIG = {
     SUPABASE_URL:        "https://oxybanupdrfopjionekk.supabase.co",
     SUPABASE_ANON:       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94eWJhbnVwZHJmb3BqaW9uZWtrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcwNTg1MjMsImV4cCI6MjA5MjYzNDUyM30.vmssHhb5u2myX29SAPb496kOfYIEBmnus5ylpe-Z7Uc",
-    SUPABASE_ANON_KEY:   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94eWJhbnVwZHJmb3BqaonekkiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTcxNTAwMDAwMCwiZXhwIjoyMDMwMDAwMDAwfQ.placeholder",
+    SUPABASE_ANON_KEY:   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94eWJhbnVwZHJmb3BqaW9uZWtrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcwNTg1MjMsImV4cCI6MjA5MjYzNDUyM30.vmssHhb5u2myX29SAPb496kOfYIEBmnus5ylpe-Z7Uc",
     SUPABASE_SERVICE_KEY: "Tq2MTpzno1DsCEH8",
     SUPERADMIN_PASSWORD: "Tq2MTpzno1DsCEH8",
     ADMIN_PASSWORD:      "Austria714#",
